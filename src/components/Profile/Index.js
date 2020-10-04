293bcc18744a4a58b3a30f92d186c24aaf1f1118
Wanderson Araujo <wanderson@intricately.com>
2020-10-04 14:13:05 -0300

    Criando primeito componente compartilhado

---
+++ src/components/Profile/Index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Wanderson Araujo</h1>
+        <h2>Full Stack Developer</h2>
+        <p>Developer full stack antenado no mercado de tecnologia, gosto de novos desafios e nas horas vagas voar com drones.</p>
+    </div>
+) 
+
+export default Profile
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Profile from "./Profile/Index"
 
 const Layout = ({ children }) => {
 
   return (
     <>
+      <aside><Profile/></aside>
       <main>{children}</main>
     </>
   )
